5c8f0d68b251e505f36a816a4d267f1c1b374e83
Sima Tolstikova <simatolstikova@gmail.com>
2026-08-26 13:41:53 +0300

    Добавлены репозитории: ClientRepository, ProductRepository

---
+++ src/main/java/com/example/demo/repository/ClientRepository.java
@@ -0,0 +1,15 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
+    Optional<Client> findByUsername(String username);
+    Optional<Client> findByEmail(String email);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
+}
