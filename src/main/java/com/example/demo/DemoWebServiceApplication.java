a233807c73c44627763fd76786392a413781d275
Sima Tolstikova <simatolstikova@gmail.com>
2026-08-26 14:52:34 +0300

    наконец-то

--- src/main/java/com/example/demo/DemoWebServiceApplication.java
+++ src/main/java/com/example/demo/DemoWebServiceApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoWebServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoWebServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(DemoWebServiceApplication.class, args);
+    }
 }
